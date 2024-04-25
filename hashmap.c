6eb43838197841a37473805618318411e00167a3
MarcosCadiz <163428866+MarcosCadiz@users.noreply.github.com>
2024-04-25 01:13:44 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -119,7 +119,7 @@ Pair * nextMap(HashMap * map) {
   if(map == NULL || map->size == 0){
     return NULL;
   }
-  int index = map->current;
+  int index = map->current + 1;
   
   while(map->buckets[index] == NULL || map->buckets[index]->key == NULL){
     index++;
