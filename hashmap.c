5e8ee1b44c147533959c2fd7441cab06a9c2d044
MarcosCadiz <163428866+MarcosCadiz@users.noreply.github.com>
2024-04-25 01:19:01 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -53,7 +53,6 @@ void insertMap(HashMap * map, char * key, void * value) {
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
     Pair ** buckets = map->buckets;
-    long size = map->size;
     long capacity = map->capacity;
     map->capacity = capacity*2;
     map->buckets = (Pair **)calloc(map->capacity,sizeof(Pair *));
