545082085cdb414c03a7d843a5f5fd97bd1cbb70
MarcosCadiz <163428866+MarcosCadiz@users.noreply.github.com>
2024-04-25 01:18:29 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -52,6 +52,21 @@ void insertMap(HashMap * map, char * key, void * value) {
 
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
+    Pair ** buckets = map->buckets;
+    long size = map->size;
+    long capacity = map->capacity;
+    map->capacity = capacity*2;
+    map->buckets = (Pair **)calloc(map->capacity,sizeof(Pair *));
+    map->size = 0;
+    for(long i=0;i<capacity;i++){
+        if(buckets[i]!=NULL && buckets[i]->key!=NULL){
+            insertMap(map,buckets[i]->key,buckets[i]->value);
+            
+        }
+        
+    }
+    free(buckets);
+    
 
 
 }
