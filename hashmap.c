2218ae25844e829afd571c5f423e59a3be10cbf2
MarcosCadiz <163428866+MarcosCadiz@users.noreply.github.com>
2024-04-25 00:58:30 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -80,7 +80,9 @@ void eraseMap(HashMap * map,  char * key) {
 }
 
 Pair * searchMap(HashMap * map,  char * key) {
-  if(map = NULL || key == NULL) return NULL;
+  if(map == NULL || key == NULL){
+    return NULL;
+  }
   int index = hash(key, map->capacity);
   while(map->buckets[index] != NULL && map->buckets[index]->key != NULL){
     if(is_equal(map->buckets[index]->key, key)){
